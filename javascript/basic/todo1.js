4c5882686dc51a271d2ad9fa636efbbd102dbd7c
rlawlstjd <kjsng20@gmail.com>
2025-01-14 21:41:33 +0900

    ,

--- javascript/basic/todo1.js
+++ javascript/basic/todo1.js
@@ -7,9 +7,9 @@
 
  let test = 9779345;
  let seconds = parseInt(test % 60) ; 
- let minutes = parseInt(seconds / 60) % 60;
- let hours = parseInt(test / 60 / 60 ) % 60; 
- let days = parseInt(test / 60 / 60 / 24); 
+ let minutes = parseInt(test / 60 % 60 )  ;
+ let hours = parseInt(test / 60 / 60 % 24); 
+ let days = parseInt(test / 60 / 60 / 24 % 365); 
 
 
  console.log(`${days}일 ${hours}시간 ${minutes}분 ${seconds}초`)
